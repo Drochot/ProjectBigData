89aff4da07e804826cf7d3fe22f2b67d38c7fe10
Ido Hiemstra <ido.hiemstra@student.nhlstenden.com>
2020-06-04 12:16:53 +0200

    Clone

---
+++ BigDataProject/tsv_to_csv_parser_v0.2/src/main/java/com/mycompany/parser/Data.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.parser;
+
+/**
+ *
+ * @author darkblade
+ */
+public class Data{
+    
+    String line;
+    public Data(String line){
+        this.line = line;
+    }
+    
+    //replaces , with ","
+    void replaceCommas() {
+        line = line.replaceAll(",", "\",\"");
+        
+    }
+    
+    //replaces (tab)    with ,
+    void replaceTabs() {
+        line = line.replaceAll("\\t", ",");
+    }
+    void replaceHooks() {
+        line = line.replaceAll("[\\[\\]]", "");
+    }
+
+    void replaceQuotations() {
+        line = line.replaceAll("\"", "");
+    }
+    
+    //get line
+    String getLineString(){
+        return line;
+    }
+    
+}
